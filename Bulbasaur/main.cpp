7d5b3f0beac7ae378b6104c387128096719b7b96
AskaTien <constantine.sky@hotmail.com>
2019-05-11 01:12:52 +0800

    Update main.cpp
    1. 设置一个Renderer，作为主要渲染方式。
    2. 构建一个基本的渲染循环。以及相因的初始化与关闭。

--- Bulbasaur/main.cpp
+++ Bulbasaur/main.cpp
@@ -1,55 +1,102 @@
-//Using SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
 
-//Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+bool init();
+
+void close();
+
+SDL_Window* gWindow = nullptr;
+
+SDL_Renderer* gRenderer = nullptr;
+
+//SDL_Texture* gTexture = nullptr;
+
 int main(int argc, char* args[])
 {
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
-
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
-
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	if (!init())
 	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		printf("Failed to initialize!\n");
 	}
 
 	else
 	{
-		//Create window
-		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
+		bool bQuit = false;
+
+		SDL_Event event;
+
+		// Rendering loop
+		while (!bQuit)
+		{
+			while (SDL_PollEvent(&event) != 0)
+			{
+				if (event.type == SDL_QUIT)
+				{
+					bQuit = true;
+				}
+			}
+
+			SDL_RenderClear(gRenderer);
+
+			SDL_RenderPresent(gRenderer);
+		}
+	}
+
+	close();
+
+	return 0;
+}
+
+bool init()
+{
+	// Initialization flag
+	bool bSuccess = true;
+
+	// Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		bSuccess = false;
+	}
+
+	else
+	{
+		// Create window
+		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == nullptr)
 		{
 			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			bSuccess = false;
 		}
 
 		else
 		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface(window);
+			// Create renderer for window
+			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+			if (gRenderer == nullptr)
+			{
+				printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+				bSuccess = false;
+			}
 
-			//Fill the surface white
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-
-			//Update the surface
-			SDL_UpdateWindowSurface(window);
-
-			//Wait two seconds
-			SDL_Delay(2000);
+			else
+			{
+				SDL_SetRenderDrawColor(gRenderer, 0x29, 0x6A, 0x54, 0xFF);
+			}
 		}
 	}
 
-	//Destroy window
-	SDL_DestroyWindow(window);
+	return bSuccess;
+}
 
-	//Quit SDL subsystems
+void close()
+{
+	// Destroy window
+	SDL_DestroyWindow(gWindow);
+	gWindow = nullptr;
+
+	// Quit SDL subsystems
 	SDL_Quit();
-
-	return 0;
 }
